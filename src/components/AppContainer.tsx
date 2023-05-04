5e8d9bd3584d8bb91ea7aa2c6956ae0a6aaf8728
prasetyoow <praztamer@gmail.com>
2023-05-04 17:07:08 +0700

    added template layouts

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../layouts/Header'
+import Body from '../layouts/Body'
+import Footer from '../layouts/Footer'
+
+export default function AppContainer() {
+  return (
+    <>
+    <div>AppContainer</div>
+    {/* Header */}
+    <Header />
+    {/* Body */}
+    <Body />
+    {/* Footer */}
+    <Footer />
+    </>
+  )
+}
