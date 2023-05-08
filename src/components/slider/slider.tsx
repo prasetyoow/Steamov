2d4890be7ffe4904eda5047455167f9c5ae54c59
prasetyoow <praztamer@gmail.com>
2023-05-08 17:00:12 +0700

    added style for slick slider carousel

---
+++ src/components/slider/slider.tsx
@@ -0,0 +1,44 @@
+import './slider.css'
+
+import Slick, { Settings } from 'react-slick'
+
+interface Props extends Settings {
+  isMovieCard?: boolean
+}
+
+export default function Slider(props: Props) {
+  let settings: Settings = {
+    ...props,
+  }
+
+  if (props.isMovieCard) {
+    settings = {
+      ...settings,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      swipe: false,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+      ],
+    }
+  }
+  return (
+    <Slick {...settings} autoplaySpeed={5000}>
+      {props.children}
+    </Slick>
+  )
+}
