09d6fc36a58eccc4f89b17788ab6d3e7c97b94fe
prasetyoow <praztamer@gmail.com>
2023-05-09 18:24:50 +0700

    creating routes for pages

--- src/layouts/Body.tsx
+++ src/layouts/Body.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/home'
+import Catalog from '../pages/catalog'
+import Film from '../pages/film'
 
 export default function Body() {
   return (
-    <div>Body</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Catalog type="movie" />} />
+      <Route path="/tv" element={<Catalog type="tv" />} />
+      <Route path="/search" element={<Catalog type="search" />} />
+
+      <Route path="/movie:id" element={<Film mediaType="movie" />} />
+      <Route path="/tv:id" element={<Film mediaType="tv" />} />
+    </Routes>
   )
 }
